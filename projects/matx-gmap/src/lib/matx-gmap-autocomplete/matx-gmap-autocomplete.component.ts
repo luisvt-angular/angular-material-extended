f5322b84a9c5cf96fae1487c3ac3f827575ba9fb
luvargas <luvargas@greathealthworks.com>
2019-02-22 19:02:33 -0500

    0.4.1 - add README to dist

--- projects/matx-gmap/src/lib/matx-gmap-autocomplete/matx-gmap-autocomplete.component.ts
+++ projects/matx-gmap/src/lib/matx-gmap-autocomplete/matx-gmap-autocomplete.component.ts
@@ -24,7 +24,6 @@ export class MatxGmapAutocompleteComponent extends DefaultValueAccessor implemen
 
   autocompleteService;
 
-  /** @type google.maps.Geocoder */
   geocoder;
   options = [];
   textBoxControl = new FormControl();
